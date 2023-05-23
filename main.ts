c65c789aa9631544a964bdb9ed6fcd5671876193
FernandoColchones <130452951+FernandoColchones@users.noreply.github.com>
2023-05-23 11:43:24 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(steps)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(46 * steps)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    steps = 0
+    basic.showNumber(0)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+let steps = 0
+steps = 0
+basic.showNumber(0)
+basic.pause(2000)
+basic.clearScreen()
+basic.forever(function () {
+    if (input.acceleration(Dimension.Strength) > 1500) {
+        steps += 1
+        basic.pause(100)
+    }
+})
